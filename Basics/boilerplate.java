e3b63d433425699755eb470fb45ec5f5e695907d
pxxja <poojabhambid2004@gmail.com>
2026-05-08 22:18:09 +0530

    Add basic Java program that prints "Hello, World!" to the console

---
+++ Basics/boilerplate.java
@@ -0,0 +1,7 @@
+// This is a basic Java program that prints "Hello, World!" to the console.
+// The class name must match the file name (boilerplate.java).
+public class boilerplate {
+    public static void main(String[] args) { // The main method is the entry point of the program.
+        System.out.println("Hello, World!");
+    }
+}   
