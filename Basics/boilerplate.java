b4593817ed27447455159306896215f11170a157
Pooja Sunil Bhambid <pooja.b@elementree.co.in>
2026-05-09 20:13:04 +0530

    Update boilerplate.java to include additional print statement

--- Basics/boilerplate.java
+++ Basics/boilerplate.java
@@ -2,6 +2,7 @@
 // The class name must match the file name (boilerplate.java).
 public class boilerplate {
     public static void main(String[] args) { // The main method is the entry point of the program.
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");// here we can also use \n for new line
+        System.out.print("Print output on the same line. ");
     }
 }   
