30c23c0ee27401c27b52d7fb2b376e8d1e7f3ed2
Pooja Bhambid <poojabhambid2004@gmail.com>
2026-05-11 20:19:53 +0530

    feat(dataTypes): add basic Java data types example
    - Introduce byte, char, boolean, float, int, long, double, and short types
    - Print values for byte and char to demonstrate usage
    - Provide comments indicating memory size and value ranges
    - Show initialization syntax for each primitive type in Java

---
+++ Basics/dataTypes.java
@@ -0,0 +1,19 @@
+// java is typed language means we've to first specify the type of value before initialzing it
+// Ex. 
+// int a=10; //here int is a 
+
+
+public class dataTypes{
+    public static void main(String args[]){
+        byte b=8;  //size= 1byte [-128 to 127]
+        System.out.println(b);
+        char ch='a'; //size= 2byte 
+        System.out.println(ch);
+        boolean var=false; //size= 1byte
+        float price=10.90; //size= 4byte
+        int num=10; //size= 4byte
+        long num1=100; //size= 8byte
+        double num2=4979; //size= 8byte
+        short num3=35; //size= 2byte
+    }
+}
