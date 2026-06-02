e60bed1275a869929403baaf52a409fcef40d7af
Pooja Bhambid <poojabhambid2004@gmail.com>
2026-06-02 20:46:46 +0530

    feat(patternProgramming): add star pattern printing program
    - Implement star.java to print a right-angled triangle of stars

---
+++ patternProgramming/star.java
@@ -0,0 +1,11 @@
+public class star{
+    public static void main(String args[]){
+        int range=5;
+        for(int i=1;i<range;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
