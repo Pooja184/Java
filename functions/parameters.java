b91c86cb0954cc5cca693df7544fee88f7a9a5cf
Pooja Bhambid <poojabhambid2004@gmail.com>
2026-06-07 19:48:56 +0530

    feat(parameters): add sum function to calculate sum of two numbers

---
+++ functions/parameters.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class parameters{
+    public static int sumOFTwoNum(int num1,int num2) {
+        int sum=num1+num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2= sc.nextInt();
+        System.out.println(sumOFTwoNum(num1,num2));
+    }
+}
